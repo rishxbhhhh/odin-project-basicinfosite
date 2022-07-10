dd25565803b3717cc4736b7eddba03872980ed84
Rishabh Rajpurohit <rajpurohitrishabh1@gmail.com>
2022-07-10 16:26:28 +0530

    minor changes

--- index.js
+++ index.js
@@ -10,37 +10,39 @@ const htmlFiles = files
   })
   .map((file) => {
     if (file.name.endsWith(".html")) {
-      return file.name.slice(0, -5);
-    } else {
-      return file.name;
-    }
+        if (file.name === "index.html") {
+          return "";
+        } else {
+          return file.name.slice(0, -5);
+        }
+      } else {
+        return file.name;
+      }
   });
 
-console.log(files);
-console.log(`HTML Files: ${htmlFiles}`);
 
 const port = process.env.port || 3000;
 
 const server = http.createServer((req, res) => {
-  const requestedURL = req.url.slice(1);
-  if (htmlFiles.includes(requestedURL)) {
-    if (requestedURL.endsWith(".css")) {
-      const cssFile = fs.readFileSync("./" + requestedURL, "utf8");
-      res.setHeader("Content-Type", "text/css");
-      res.end(cssFile);
-    } else {
-      if (requestedURL === "") {
-        const file = fs.readFileSync("./index.html", "utf8");
-        res.end(file);
+    const requestedURL = req.url.slice(1);
+    if (htmlFiles.includes(requestedURL)) {
+      if (requestedURL.endsWith(".css")) {
+        const cssFile = fs.readFileSync("./" + requestedURL, "utf8");
+        res.setHeader("Content-Type", "text/css");
+        res.end(cssFile);
       } else {
-        const file = fs.readFileSync("./" + requestedURL + ".html");
-        res.end(file);
+        if (requestedURL === "") {
+          const file = fs.readFileSync("./index.html", "utf8");
+          res.end(file);
+        } else {
+          const file = fs.readFileSync("./" + requestedURL + ".html");
+          res.end(file);
+        }
       }
+    } else {
+      const file = fs.readFileSync("./404.html", "utf8");
+      res.end(file);
     }
-  } else {
-    const file = fs.readFileSync("./404.html", "utf8");
-    res.end(file);
-  }
 });
 
 server.listen(port, () => {
