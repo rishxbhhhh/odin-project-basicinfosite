a02c9e01ceee5e4bccbb4373dad8b1bfd3986ea9
Rishabh Rajpurohit <rajpurohitrishabh1@gmail.com>
2022-07-10 15:00:00 +0530

    added pages

---
+++ index.js
@@ -0,0 +1,48 @@
+const http = require("http");
+const fs = require("fs");
+
+const files = fs.readdirSync("./", { withFileTypes: true });
+const htmlFiles = files
+  .filter((file) => {
+    if (file.name.endsWith(".html") || file.name.endsWith(".css")) {
+      return file.name;
+    }
+  })
+  .map((file) => {
+    if (file.name.endsWith(".html")) {
+      return file.name.slice(0, -5);
+    } else {
+      return file.name;
+    }
+  });
+
+console.log(files);
+console.log(`HTML Files: ${htmlFiles}`);
+
+const port = process.env.port || 3000;
+
+const server = http.createServer((req, res) => {
+  const requestedURL = req.url.slice(1);
+  if (htmlFiles.includes(requestedURL)) {
+    if (requestedURL.endsWith(".css")) {
+      const cssFile = fs.readFileSync("./" + requestedURL, "utf8");
+      res.setHeader("Content-Type", "text/css");
+      res.end(cssFile);
+    } else {
+      if (requestedURL === "") {
+        const file = fs.readFileSync("./index.html", "utf8");
+        res.end(file);
+      } else {
+        const file = fs.readFileSync("./" + requestedURL + ".html");
+        res.end(file);
+      }
+    }
+  } else {
+    const file = fs.readFileSync("./404.html", "utf8");
+    res.end(file);
+  }
+});
+
+server.listen(port, () => {
+  console.log(`Server listening on Port: ${port}`);
+});
